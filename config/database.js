be063f928be3fe82cb8e260566dabf4d13cb1d7b
jbalata <jbalata@employeenavigator.com>
2020-02-06 16:33:38 -0500

    attempting to connect to mongo with mLab

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = () => {
   // Default to dev presets
   const dbConfig = {
-    url: 'mongodb://localhost:27017/crosswordsDb',
+    //url: 'mongodb://localhost:27017/crosswordsDb',
+    url: 'mongodb://heroku_t380f0c8:osuscholar91@ds045897.mlab.com:45897/heroku_t380f0c8',
     opts: {
       useMongoClient: true,
       autoReconnect: true,
@@ -11,7 +12,8 @@ module.exports = () => {
 
   switch (process.env.NODE_ENV) {
     case 'production':
-      Object.assign(dbConfig, { url: process.env.MONGODB_URI || 'mongodb://localhost:27017/crosswordDb' });
+      Object.assign(dbConfig, {url: 'mongodb://heroku_t380f0c8:osuscholar91@ds045897.mlab.com:45897/heroku_t380f0c8'})
+      //Object.assign(dbConfig, { url: process.env.MONGODB_URI || 'mongodb://localhost:27017/crosswordDb' });
       break;
     case 'stage':
       break;
