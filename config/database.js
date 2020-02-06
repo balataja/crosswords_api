1562aaa80eff130d2a1793a451f3e7aa813f15e2
jbalata <jbalata@employeenavigator.com>
2020-02-06 16:50:47 -0500

    I can connect now

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = () => {
   // Default to dev presets
   const dbConfig = {
     //url: 'mongodb://localhost:27017/crosswordsDb',
-    url: 'mongodb://heroku_t380f0c8:osuscholar91@ds045897.mlab.com:45897/heroku_t380f0c8',
+    url: 'mongodb://jamesUsr:osuscholar91@ds045897.mlab.com:45897/heroku_t380f0c8',
     opts: {
       useMongoClient: true,
       autoReconnect: true,
