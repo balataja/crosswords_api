f9946ee91de5a7c10d0345c40b811cd2e03bf68e
jbalata <jbalata@employeenavigator.com>
2020-02-06 15:22:32 -0500

    messing with cors

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ const app = new Koa()
   .use(bodyParser())
   .use(routes);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 const server = http.createServer(app.callback())
 const io = socket(server)
 
