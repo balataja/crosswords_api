c2b0aeda3857ff11f48dd0cbe29a0af330f744a4
jbalata <jbalata@employeenavigator.com>
2020-02-06 16:07:39 -0500

    cors test

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ mongoose.Promise = global.Promise;
 mongoose.connect(config.database.url, config.database.opts);
 
 const app = new Koa()
-  //.use(cors())
-  .use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-  })
+  .use(cors())
+  // .use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", '*');
+  //   res.header("Access-Control-Allow-Credentials", true);
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  //   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  //   next();
+  // })
   .use(logger())
   .use(bodyParser())
   .use(routes);
