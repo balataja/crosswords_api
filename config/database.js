655a6c67b98f3c5d0d90c44b311d6c3dbdc9c0fb
jbalata <jbalata@employeenavigator.com>
2020-12-29 12:24:45 -0500

    updating atlas connection strings

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ module.exports = () => {
   // Default to dev presets
   const dbConfig = {
     //url: 'mongodb://localhost:27017/crosswordsDb',
-    url: 'mongodb://jamesUsr:test1234@ds045897.mlab.com:45897/heroku_t380f0c8',
+    //url: 'mongodb://jamesUsr:test1234@ds045897.mlab.com:45897/heroku_t380f0c8',
+    url: 'mongodb://jamesUsr:test1234@cluster-t380f0c8-shard-00-00.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-01.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-02.hwlnz.mongodb.net:27017/heroku_t380f0c8?ssl=true&replicaSet=atlas-55brtd-shard-0&authSource=admin&retryWrites=true&w=majority',
     opts: {
       useMongoClient: true,
       autoReconnect: true,
@@ -13,7 +14,7 @@ module.exports = () => {
   switch (process.env.NODE_ENV) {
     case 'production':
       //Object.assign(dbConfig, {url: 'mongodb://jamesUsr:test1234@ds045897.mlab.com:45897/heroku_t380f0c8'})
-      Object.assign(dbConfig, {url: 'mongodb+srv://jamesUsr:test1234@cluster-t380f0c8.hwlnz.mongodb.net/heroku_t380f0c8?retryWrites=true&w=majority'})
+      Object.assign(dbConfig, {url: 'mongodb://jamesUsr:test1234@cluster-t380f0c8-shard-00-00.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-01.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-02.hwlnz.mongodb.net:27017/heroku_t380f0c8?ssl=true&replicaSet=atlas-55brtd-shard-0&authSource=admin&retryWrites=true&w=majority'})
       //mongodb+srv://<username>:<password>@cluster-t380f0c8.hwlnz.mongodb.net/heroku_t380f0c8?retryWrites=true&w=majority
       //mongodb://<username>:<password>@cluster-t380f0c8-shard-00-00.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-01.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-02.hwlnz.mongodb.net:27017/heroku_t380f0c8?ssl=true&replicaSet=atlas-55brtd-shard-0&authSource=admin&retryWrites=true&w=majority
       //Object.assign(dbConfig, { url: process.env.MONGODB_URI || 'mongodb://localhost:27017/crosswordDb' });
@@ -21,6 +22,7 @@ module.exports = () => {
     case 'stage':
       break;
     case 'test':
+      //Object.assign(dbConfig, {url: 'mongodb://jamesUsr:test1234@cluster-t380f0c8-shard-00-00.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-01.hwlnz.mongodb.net:27017,cluster-t380f0c8-shard-00-02.hwlnz.mongodb.net:27017/heroku_t380f0c8?ssl=true&replicaSet=atlas-55brtd-shard-0&authSource=admin&retryWrites=true&w=majority'})
       Object.assign(dbConfig, { url: 'mongodb://localhost:27017/crosswordDb' });
       break;
     case 'dev':
